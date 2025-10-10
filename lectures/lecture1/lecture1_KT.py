3fb6ffbfdd97f46a9976ee45a3081e3450a8df8b
Kai Thomas <kai.thomas@student.nhh.no>
2025-10-10 16:57:44 +0200

    lecutre 1 changes

---
+++ lectures/lecture1/lecture1_KT.py
@@ -0,0 +1,51 @@
+"""
+Part 2, Lecture 1
+
+Implement and test argmax() function that returns the location of a maximum.
+
+Tasks
+-----
+
+1.  Implement a function argmax() that takes a sequence of numbers and returns 
+    the index (position) the maximum element.
+
+2.  Test the function with the following sequence of numbers:
+    [2, 3, -1, 7, 4]
+
+3.  Add error handling if an empty sequence is passed. Test the function with an 
+    empty sequence.
+
+4.  Use the notebook lecture1-benchmark.ipynb to benchmark your implementation 
+    against NumPy's argmax().
+"""
+
+import numpy as np
+
+def argmax(lst):
+    
+    N = len(lst)
+
+    if N == 0:
+        raise ValueError('Empty list are not supported')
+
+    value_max = - np.inf
+    imax = 0
+
+    for i in range (N):
+        value = lst [i]
+        if value > value_max:
+            # Upadte index of largest element
+            imax = i
+            # Update the largest element
+            value_max = value
+    return imax
+
+if __name__ == '__main__':
+    values = [20, 3, -1, 7, 4]
+    # values = []
+
+    try:
+        i = argmax(values)    
+        print(f"Index of max value: {i}")
+    except ValueError:
+        print("Empty list")
